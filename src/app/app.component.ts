4a0156fa9704a835894218c4990f6757921c4062
Edward Navarrete <edward.navarrete@elkayinteriorsystems.com>
2023-06-05 14:49:59 -0600

    Creando un Nuevo Componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   ngOnInit(): void {
     setTimeout(()=>{
-      this.peliculas = [{
-        titulo: 'Spider Man',
-        fechaLanzamiento: new Date(),
-        precio: 1400.99
-      },
-      {
-        titulo: 'Moana',
-        fechaLanzamiento: new Date('2016-11-14'),
-        precio: 300.99
-      }]
+      
     },3000);
   }
   title = 'El valor que sea xd';
