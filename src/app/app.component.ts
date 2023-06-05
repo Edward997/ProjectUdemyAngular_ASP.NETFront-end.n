dc71153dde4bb50770d7b438cb1e859da2b394e8
Edward Navarrete <edward.navarrete@elkayinteriorsystems.com>
2023-06-05 14:10:28 -0600

    proyecto frontend

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    setTimeout(()=>{
+      this.peliculas = [{
+        titulo: 'Spider Man',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'Moana',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 300.99
+      }]
+    },3000);
+  }
+  title = 'El valor que sea xd';
+
+  peliculas;
+
+  duplicarNumero(valor: number): number{
+    return valor * 2;
+  }  
+}
