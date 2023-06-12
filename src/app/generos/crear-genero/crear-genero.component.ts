1cb1cb32ee9ce19892404856afe9bfb9b8fe4958
Edward Navarrete <edward.navarrete@elkayinteriorsystems.com>
2023-06-12 17:14:51 -0600

    Validaciones en formularios Reactivos

--- src/app/generos/crear-genero/crear-genero.component.ts
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,12 +9,33 @@ import { Router } from '@angular/router';
 })
 export class CrearGeneroComponent {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
+
+  form: FormGroup;
+  
+  ngOnInit(): void{
+    this.form = this.formBuilder.group({
+      nombre: ['', {
+        validators: [Validators.required, Validators.minLength(3)]
+      }]
+    });
+  }
 
   guardarCambios(){
     // ... guardar los cambios
-    this.router.navigate(['/generos'])
-
+    this.router.navigate(['/generos']);
   }
 
+  obtenerErrorCampoNombre(){
+    var campo = this.form.get('nombre');
+    if (campo.hasError('required')){
+      return 'El campo nombre es requerido';
+    }
+
+    if (campo.hasError('minlength')){
+      return 'La longitud minima es de 3 caracteres';
+    }
+
+    return '';
+  }
 }
