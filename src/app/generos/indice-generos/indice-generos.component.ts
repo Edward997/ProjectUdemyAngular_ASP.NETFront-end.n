7d2c2add40e80922699f07a2c3e932004d25e52f
Edward997 <edward.navarrete7@gmail.com>
2023-07-06 09:04:45 -0600

    No muestra tabla

--- src/app/generos/indice-generos/indice-generos.component.ts
+++ src/app/generos/indice-generos/indice-generos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GenerosService } from '../generos.service';
 import { generoDTO } from '../genero';
 import { HttpResponse } from '@angular/common/http';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-indice-generos',
@@ -19,12 +20,20 @@ export class IndiceGenerosComponent implements OnInit {
   cantidadRegistrosAMostrar = 10;
 
   ngOnInit():void {
-    this.generosService.obtenerTodos(this.paginaActual, this.cantidadRegistrosAMostrar)
+    
+  }
+
+  cargarRegistros(pagina: number, cantidadElementosAMostrar){
+    this.generosService.obtenerTodos(pagina, cantidadElementosAMostrar)
     .subscribe((respuesta: HttpResponse<generoDTO[]>) => {
       this.generos = respuesta.body;
-      console.log(respuesta.headers.get("cantidadTotalRegistros"));
       this.cantidadTotalRegistros = respuesta.headers.get("cantidadTotalRegistros");
     }, error => console.error(error));
-     
+  }
+
+  actualizarPaginacion(datos: PageEvent) {
+    this.paginaActual = datos.pageIndex + 1;
+    this.cantidadRegistrosAMostrar = datos.pageSize;
+    this.cargarRegistros(this.paginaActual, this.cantidadRegistrosAMostrar);
   }
 }
